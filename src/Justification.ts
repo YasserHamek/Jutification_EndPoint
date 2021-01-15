c62cd042d951e3f7ff24c72aae564dd6a9c4add5
Yasser <yasser.hamek@gmail.com>
2021-01-15 16:35:58 +0100

    .

--- src/Justification.ts
+++ src/Justification.ts
@@ -22,7 +22,7 @@ export class Justification {
         //calling justifyLine method
         this.textArray.forEach((value) => this.finalText.push(this.justifyLine(value)));
         
-        return this.textArray;
+        return this.finalText;
     }
 
     //slice the text in small texts, return an array of small texts 
@@ -74,28 +74,29 @@ export class Justification {
     justifyLine(line: string): string{
         let outPutLine = line;
         let numberOfSpan: number = 0;
-
+        
         if(outPutLine.charAt(outPutLine.length)=='\n'){
             return outPutLine;
         }else {
 
-            outPutLine.trimEnd;
-            numberOfSpan = outPutLine.length-80;
+            numberOfSpan = 80 - outPutLine.length;
 
             if(numberOfSpan==0){
                 return outPutLine;
             }else {
-                return outPutLine = this.AddingSpanToLine(outPutLine,numberOfSpan);
+                outPutLine = this.AddingSpanToLine(outPutLine,numberOfSpan);
+                return outPutLine;
             }
         }
     }
 
     AddingSpanToLine(line: string, spanNumber: number): string{
+        let spanNum: number = spanNumber;
         let inPutLine: string = line;
         let outPutLine: string = '';
         let i: number = 0;
 
-        do{
+        while(i<spanNum*2){
             if(i%2==0){
                 outPutLine= outPutLine.concat(inPutLine.slice(0,inPutLine.search(' ')+1)+' ');
                 inPutLine = inPutLine.slice(inPutLine.search(' ')+1);
@@ -104,7 +105,7 @@ export class Justification {
                 inPutLine = inPutLine.slice(inPutLine.search(' ')+1);
             }
             i++;
-        } while(i<spanNumber*2)
+        }
 
         if(inPutLine.length>1){
             outPutLine= outPutLine.concat(inPutLine);
@@ -116,21 +117,23 @@ export class Justification {
 
 //some test
 let justifyClass: Justification = new Justification();
-let arrayText: string[];
+let arrayText: string[]= [];
 let text: string;
 
 text='Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte, mes yeux se fermaient si vite que je n’avais pas le temps de me dire: «Je m’endors.» Et, une demi-heure après, la pensée qu’il était temps de chercher le sommeil m’éveillait; je voulais poser le volume que je croyais avoir dans les mains et souffler ma lumière; je n’avais pas cessé en dormant de faire des réflexions sur ce que je venais de lire, mais ces réflexions avaient pris un tour un peu particulier; il me semblait que j’étais moi-même ce dont parlait l’ouvrage: une église, un quatuor, la rivalité de François Ier et de Charles-Quint. \n Cette croyance survivait pendant quelques secondes à mon réveil; elle ne choquait pas ma raison, mais pesait comme des écailles sur mes yeux et les empêchait de se rendre compte que le bougeoir n’était plus allumé. Puis elle commençait à me devenir inintelligible, comme après la métempsycose les pensées d’une existence antérieure; le sujet du livre se détachait de moi, j’étais libre de m’y appliquer ou non; aussitôt je recouvrais la vue et j’étais bien étonné de trouver autour de moi une obscurité, douce et reposante pour mes yeux, mais peut-être plus encore pour mon esprit, à qui elle apparaissait comme une chose sans cause, incompréhensible, comme une chose vraiment obscure. Je me demandais quelle heure il pouvait être; j’entendais le sifflement des trains qui, plus ou moins éloigné, comme le chant d’un oiseau dans une forêt, relevant les distances, me décrivait l’étendue de la campagne déserte où le voyageur se hâte vers la station prochaine; et le petit chemin qu’il suit va être gravé dans son souvenir par l’excitation qu’il doit à des lieux nouveaux, à des actes inaccoutumés, à la causerie récente et aux adieux sous la lampe étrangère qui le suivent encore dans le silence de la nuit, à la douceur prochaine du retour.'
-console.log(text.length)
+
+let text2='hello algorithme justification hello algorithme justification hello algorithme justification hello algorithme justification hello algorithme justification hello algorithme justification hello algorithme justification '
+//console.log(text.length)
 
 //addingSpans
 //console.log(justifyClass.AddingSpanToLine(text,4))
 
 //justification
-let paragraphe = justifyClass.MainJustificationMethod(text);
-
-console.log(paragraphe);
-console.log(paragraphe.length)
-
+//let paragraphe = justifyClass.MainJustificationMethod(text);
+let paragraphe2 = justifyClass.AddingSpanToLine(text2,10);
+let arr: string[] = [];
+arr.push('  paragraphe  '.trim())
+console.log(arr);
 
 //Slicing Text Test
 /*
